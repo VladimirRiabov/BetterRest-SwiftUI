611e00d41d5ddf804d51d7d10ebf80c84427881d
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-10 11:00:12 +0300

    Initial Commit

---
+++ BetterRest SwiftUI/BetterRest_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRest_SwiftUIApp.swift
+//  BetterRest SwiftUI
+//
+//  Created by Владимир Рябов on 10.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRest_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
